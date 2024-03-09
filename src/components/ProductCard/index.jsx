6f1990a8539dca227ddb0293938781b856853ceb
Tamilvanan <tamilvanan@doodleblue.com>
2024-03-10 00:32:59 +0530

    product card responsive changes

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -10,39 +10,36 @@ const ProductCard = ({ product }) => {
   ).toFixed(2);
 
   return (
-    <div className="max-w-sm bg-white rounded-lg overflow-hidden border shadow hover:shadow-lg transition-shadow duration-300">
+    <div class="max-w-sm bg-white rounded-lg overflow-hidden border shadow hover:shadow-lg transition-shadow duration-300 flex flex-col h-full">
       <div className="relative">
         <LazyImage
           src={product.thumbnail}
           alt={product.title}
-          className="w-full h-[20rem]"
+          className="w-full md:h-48 !h-[12rem]"
         />
-        <button className="absolute top-2.5 right-2.5 p-2 text-white bg-gray-500 hover:bg-gray-600 transition-colors duration-300">
+        <button className="absolute top-2 right-2 p-1 sm:p-2 text-white bg-gray-500 hover:bg-gray-600 transition-colors duration-300 text-xs sm:text-base">
           <AiOutlineHeart />
         </button>
       </div>
-      <div className="px-5 py-3">
-        <h5 className="text-gray-900 font-bold mb-2 text-left">
+      <div className="md:px-4 px-2 sm:px-5 py-2 sm:py-3">
+        <h5 className="text-gray-900 font-bold mb-1 sm:mb-2 text-left text-xs sm:text-lg leading-tight text-ellipsis line-clamp-1">
           {product.title}
         </h5>
-        <p className="text-gray-700 text-base mb-2 overflow-hidden text-ellipsis line-clamp-2 text-left">
+        <p className="text-gray-700 text-xs sm:text-base mb-1 sm:mb-2 overflow-hidden text-ellipsis line-clamp-2 text-left leading-snug">
           {product.description}
-        </p>{" "}
+        </p>
         <div className="flex justify-between items-center">
           <div>
-            <span className="text-gray-900 font-bold text-xl">
+            <span className="text-gray-900 font-bold text-lg sm:text-xl">
               ${discountedPrice}
-            </span>
-            <span className="line-through text-gray-400 text-sm ml-2">
-              ${product.price.toFixed(2)}
             </span>
           </div>
         </div>
-        <div className="flex items-center mt-2.5">
+        <div className="flex items-center mt-2">
           {Array.from({ length: 5 }, (_, index) => (
             <FaStar
               key={index}
-              className={`w-5 h-5 ${
+              className={`w-4 h-4 sm:w-5 sm:h-5 ${
                 index < Math.floor(product.rating)
                   ? "text-yellow-400"
                   : "text-gray-300"
--- src/screens/Products/index.jsx
+++ src/screens/Products/index.jsx
@@ -120,9 +120,11 @@ const Products = () => {
           {productData?.length === 0 ? (
             <NoRecord />
           ) : (
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 py-4">
+            <div class="grid grid-cols-2 md:grid-cols-4 gap-4 my-2">
               {productData?.map((product) => (
-                <ProductCard key={product.id} product={product} />
+                <div key={product.id}>
+                  <ProductCard product={product} />
+                </div>
               ))}
             </div>
           )}
